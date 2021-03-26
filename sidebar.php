66c9b60e12a25eeded7fcdb67ae9974d50a76f6c
Дмитрий <62281111+dmitry-juda@users.noreply.github.com>
2021-03-26 10:23:30 +0300

    Чуть более ровный вариант натяжки

--- sidebar.php
+++ sidebar.php
@@ -20,34 +20,57 @@
 <td class="content_right">
                         <div class="content_right_block">
                             <h3>Новости</h3>
+                            <?
+                            include_once('classes/simple_html_dom.php');
+
+                            $htmlDL = file_get_contents('https://1cbo.ru/o_kompanii/novosti_i_aktsii/');
+                            $html = str_get_html($htmlDL);
+                            $i = 0;
+                            // Находите информацию по селектору CSS 
+                            foreach($html->find('div.feedback-item-content') as $article) {
+                                // парсите результат и закидываете в массив
+                                if($i==5) {
+                                    break;
+                                } 
+                                else {
+                                    $i = $i + 1;
+                                    $item['title']     = $article->find('a', 0)->plaintext;
+                                    $item['link']     = $article->find('a', 0)->attr['href'];
+                                    $item['articleDate']      = $article->find('div.feedback-item-date', 0)->plaintext;                                    
+                                    ?>
+                                    <div class="right_block_news-item">
+                                        <div class="news-item_date"><? echo $item['articleDate']; ?></div>
+                                        <a href="https://1cbo.ru<? echo $item['link']; ?>" target="_blank"><? echo $item['title']; ?></a>
+                                    </div>
+                                    <?
+                                }
+                            }
+
+                            //print_r($articles);
+                            ?>
+                            <!--
                             <div class="right_block_news-item">
                                 <div class="news-item_date">13.01.2102</div>
                                 <a href="news.html">текст вашей новости, текст вашей новости, текст вашей новости</a>
                             </div>
-                            <div class="right_block_news-item">
-                                <div class="news-item_date">13.01.2102</div>
-                                <a href="news.html">текст вашей новости, текст вашей новости, текст вашей новости</a>
-                            </div>
-                            <div class="right_block_news-item">
-                                <div class="news-item_date">13.01.2102</div>
-                                <a href="news.html">текст вашей новости, текст вашей новости, текст вашей новости</a>
-                            </div>
-                            <div class="right_block_news-item">
-                                <div class="news-item_date">13.01.2102</div>
-                                <a href="news.html">текст вашей новости, текст вашей новости, текст вашей новости</a>
-                            </div>
-                            <div class="right_block_news-item">
-                                <div class="news-item_date">13.01.2102</div>
-                                <a href="news.html">текст вашей новости, текст вашей новости, текст вашей новости</a>
-                            </div>
+                            -->
                         </div>
                         <div class="content_right_block">
                             <h3>Контакты</h3>
-                            <p>241035 г. Нижний Новгород, ул. Протасова, 1, 2-й этаж, офис 9 </p>
-                            <p>+7 (383) 325-39-32</p>
-                            <a href="mailto:mail@1c-bryansk.ru">mail@1c-bryansk.ru</a>
+                            <p>	
+                                <?php //echo "adress"; ?>
+                                Город Краснодар, улица Калинина 327, офис 403
+                            </p>
+                            <p>
+                                <?php //echo "phone number"; ?>
+
+                                8 (861) 279 00-72
+                            </p>
+                            <a href="mailto:sva@mtmb.ru">
+                                <?php //"email here"; ?>
+                                E-mail?
+                            </a>
                             <br/><br/>
                             <h5>Как нас найти:</h5>
-                            <iframe width="240" height="240" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=ru&amp;geocode=&amp;q=%D1%83%D0%BB%D0%B8%D1%86%D0%B0+%D0%9F%D1%80%D0%BE%D1%82%D0%B0%D1%81%D0%BE%D0%B2%D0%B0,+1,+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA,+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=60.806372,135.263672&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=%D1%83%D0%BB.+%D0%9F%D1%80%D0%BE%D1%82%D0%B0%D1%81%D0%BE%D0%B2%D0%B0,+1,+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA,+%D0%B3%D0%BE%D1%80%D0%BE%D0%B4+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA,+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F&amp;ll=53.298613,34.313532&amp;spn=0.00285,0.008256&amp;z=14&amp;output=embed"></iframe><!--<br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=ru&amp;geocode=&amp;q=%D1%83%D0%BB%D0%B8%D1%86%D0%B0+%D0%9F%D1%80%D0%BE%D1%82%D0%B0%D1%81%D0%BE%D0%B2%D0%B0,+1,+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA,+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=60.806372,135.263672&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=%D1%83%D0%BB.+%D0%9F%D1%80%D0%BE%D1%82%D0%B0%D1%81%D0%BE%D0%B2%D0%B0,+1,+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA,+%D0%B3%D0%BE%D1%80%D0%BE%D0%B4+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA,+%D0%91%D1%80%D1%8F%D0%BD%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F&amp;ll=53.298613,34.313532&amp;spn=0.00285,0.008256&amp;z=14" style="color:#0000FF;text-align:left">Просмотреть увеличенную карту</a></small>-->
-                        </div>
+                            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A9b9184c46cdd8d8ed89e91d16cdd2463056751e3057788ac8dcbe229a849b19d&amp;source=constructor" width="320" height="240" frameborder="0"></iframe></div> 
                     </td>
